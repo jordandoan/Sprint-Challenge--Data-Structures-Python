f8f207c7d3c6dd38160a534f7323d230a0c36a11
Jordan <jordandoan@hotmail.com>
2020-01-31 09:49:05 -0800

    Stretch for names done

--- names/names.py
+++ names/names.py
@@ -11,21 +11,44 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
-cache1 = LRUCache(10000)
-cache2 = LRUCache(10000)
+# cache1 = LRUCache(10000)
+# cache2 = LRUCache(10000)
 duplicates = []
-
-for i in range(10000):
-    cache1.set(names_1[i], True)
-    cache2.set(names_2[i], True)
-
-for key1 in cache1.keys:
-    if cache2.get(key1):
-        duplicates.append(key1)
+#
+# for i in range(10000):
+#     cache1.set(names_1[i], True)
+#     cache2.set(names_2[i], True)
+#
+# for key1 in cache1.keys:
+#     if cache2.get(key1):
+#         duplicates.append(key1)
 # for name_1 in names_1:
 #     for name_2 in names_2:
 #         if name_1 == name_2:
 #             duplicates.append(name_1)
+
+names_1.sort()
+names_2.sort()
+n1 = 0
+while n1 < len(names_1)-1:
+    if names_1[n1] == names_1[n1+1]:
+        names_1.pop(n1+1)
+    else:
+        n1 += 1
+n2 = 0
+while n2 < len(names_2)-1:
+    if names_2[n2] == names_2[n2+1]:
+        names_2.pop(n2+1)
+    else:
+        n2 += 1
+all_names = names_1 + names_2
+all_names.sort()
+for i in range(len(all_names)-1):
+    if all_names[i] == all_names[i+1]:
+        duplicates.append(all_names[i])
+        i += 2
+
+
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
