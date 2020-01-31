18b3bcbfe910aa7c49c5839019e1ba6405fa748c
Jordan <jordandoan@hotmail.com>
2020-01-31 09:10:01 -0800

    Problem 2 done

--- names/names.py
+++ names/names.py
@@ -1,6 +1,7 @@
 import time
-
+from lru_cache import LRUCache
 start_time = time.time()
+
 
 f = open('names_1.txt', 'r')
 names_1 = f.read().split("\n")  # List containing 10000 names
@@ -10,11 +11,21 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+cache1 = LRUCache(10000)
+cache2 = LRUCache(10000)
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+
+for i in range(10000):
+    cache1.set(names_1[i], True)
+    cache2.set(names_2[i], True)
+
+for key1 in cache1.keys:
+    if cache2.get(key1):
+        duplicates.append(key1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
